1b5f5ad14dc4b8c0175663b16ebd1ace5b3c9a72
xu9449 <xu9449@gmail.com>
2019-10-07 22:35:48 -0400

    Update 1003

--- NewStart/Assets/Scripts/RandomPosition.cs
+++ NewStart/Assets/Scripts/RandomPosition.cs
@@ -7,7 +7,7 @@ public class RandomPosition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutin
     }
 
     // Update is called once per frame
@@ -15,4 +15,11 @@ public class RandomPosition : MonoBehaviour
     {
         
     }
+
+    void SetRandomPosition()
+	{
+		float x = Random.Range(-5.0f, 5.0f);
+		float z = Random.Range(-5.0f, 5.0f);
+		transform.position = new Vector3(x, 0.0f, z);
+	}
 }
